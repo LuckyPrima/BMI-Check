1f3625afb0136b23910a40ab34a0debb53f80707
luckyprima2002@gmail.com <luckyprima@Lucky-Prima.>
2025-03-04 19:57:42 +0700

    responsive todolist

--- src/pages/todolist.tsx
+++ src/pages/todolist.tsx
@@ -43,24 +43,26 @@ function ToDoList() {
 
   return (
     <div className="to-do-list flex flex-col items-center p-10">
-      <div className="min-h-[600px] bg-[#2A3335] w-[460px] rounded-3xl p-3 drop-shadow-xl">
-        <h1 className="text-center text-3xl p-3 text-[#F1F0E9] bg-[#1F509A] rounded-2xl drop-shadow-md">
+      <div className="lg:min-h-[600px] min-h-[500px] bg-[#2A3335] lg:w-[460px] w-full rounded-3xl p-3 drop-shadow-xl">
+        <h1 className="text-center lg:text-3xl md:text-3xl text-2xl p-3 text-[#F1F0E9] bg-[#1F509A] rounded-2xl drop-shadow-md">
           To Do List
         </h1>
 
         <div className="inline-block pr-4 pt-4">
           <input
-            className="border-solid border-2 border-[#A1E3F9] w-[349px] bg-[#F1F0E9]"
+            className="border-solid border-2 border-[#A1E3F9] lg:w-[330px] md:w-[560px] w-[180px] bg-[#F1F0E9]"
             type="text"
             placeholder="Enter a task ..."
             value={newTask}
-            onChange={handleInputChange}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleInputChange(e)
+            }
           />
         </div>
 
         <div className="inline-block">
           <button
-            className="bg-[#1F509A] hover:bg-[#0A3981] p-1 rounded-xl text-[#F1F0E9]"
+            className="bg-[#1F509A] hover:bg-[#0A3981] p-1 rounded-xl md:text-lg text-base text-[#F1F0E9]"
             onClick={addTask}
           >
             Add Task
@@ -76,7 +78,7 @@ function ToDoList() {
         >
           {tasks.map((task, index) => (
             <li
-              className="border-b-2 border-[#EAEAEA] w-full p-2 flex justify-between items-center text-[#F1F0E9]"
+              className="border-b-2 border-[#EAEAEA] w-full p-2 flex justify-between items-center  md:text-lg text-base text-[#F1F0E9]"
               key={index}
             >
               {task}
@@ -94,7 +96,7 @@ function ToDoList() {
                   ⬇
                 </button>
                 <button
-                  className="hover:bg-gray-500 p-1"
+                  className="hover:bg-red-500 p-1"
                   onClick={() => deleteTask(index)}
                 >
                   ❌
